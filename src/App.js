0d36be40eb97f88b6d37a06a8ab755501982a3bb
Coyaks <coyaks19@mail.com>
2022-07-23 15:58:27 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import React, { useEffect,useState } from 'react';
 import './App.css';
 
+/**
+ * Siempre que hablemos de useState -> me voy a imaginar en el example del contador 
+ * useState -> returna una array -> [value (valor contador), setValue (funcion para actualizar el contador)]  
+ */
 function App() {
+
+  function btnGetData(){
+    console.log('Click')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="my-3" style={{textAlign: 'center'}}>
+      <button className="btn btn-success my-button" onClick={btnGetData}>Get data</button>
+      <Card></Card>
+    </div>
+  );
+}
+
+
+/*Uso de hook useEffect*/
+const Card=(props)=>{
+
+  /*Simular mi propio API*/
+  const data=[
+    {id:1, name: 'Coyaks'},
+    {id:2, name: 'Felix'},
+    {id:2, name: 'Paula'},
+  ];
+  /**
+   * Crear mi primer hook -> useState
+   */
+  const [valueUser, setUser]=useState([]);
+
+  useEffect(()=>{
+    console.log('useEffect success')
+    getDataUserApi()
+  },[])
+
+  const getDataUserApi= async()=>{
+    const data=await fetch('https://jsonplaceholder.typicode.com/users')
+    const users=await data.json()
+    setUser(users)
+  }
+
+  return (
+    <div className="container border shadow p-3">
+          <h3>Users</h3>
+          <ul>
+              {valueUser.map(o=>(
+                <li key={o.id}>{o.name}</li>
+              ))}
+
+          </ul>
+    </div>
+  );
+}
+
+const Saludo=(props)=>{
+  return (
+    <div>
+        <h2 className="title">Hola {props.name}</h2>
     </div>
   );
 }
